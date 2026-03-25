46cf7611ad95349658a7711aee3f433bea9ffad5
BHASKAR <bassnaidu1242@gmail.com>
2026-03-25 10:21:37 +0530

    Update api.ts

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || "/api";
+const API_URL = "https://farmconnectdirect.onrender.com/api";
 
 const getToken = () => localStorage.getItem("farmconnect_token");
 
